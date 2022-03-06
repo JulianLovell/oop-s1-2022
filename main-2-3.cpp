58625060d23d9a6f1f6c24615026a45db7dee935
JulianLovell <jlovell108@gmail.com>
2022-03-06 10:54:37 +0000

    practical1

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -1,1 +1,15 @@
-hello
+#include <iostream>
+
+int sum_array(int array[], int n) {
+    int result = 0;
+    if (n < 1) {
+    }
+    if (n >= 1) {
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total = total + array[i];
+        }
+        result = total;
+    }
+    return result;
+}

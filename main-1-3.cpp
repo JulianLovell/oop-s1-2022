31105ec82590710f28c9ee5b05308c0b21d9fd4f
JulianLovell <jlovell108@gmail.com>
2022-03-06 09:12:58 +0000

    practial1

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -3,8 +3,8 @@
 extern int count(int array[], int n, int number);
 
 int main(){
-   int array[] = {4,5,6,8, 8};
-   int n = 1;
+   int array[] = {4,5,6,8,8};
+   int n = 8;
    int number = sizeof(array)/sizeof(array[0]);
    std::cout <<"Frequency of number in array: "<< count(array, n, number);
    return 0;

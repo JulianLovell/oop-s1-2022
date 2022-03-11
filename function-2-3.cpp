1696ca33fb5e7999a9abc869ef29b2e509211251
JulianLovell <jlovell108@gmail.com>
2022-03-11 08:16:37 +0000

    practical2

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -6,3 +6,15 @@ int sum_if_a_palindrome(int integers[], int length) {
 
 }
 
+int bool is_a_palindrome(int integers[], int length) {
+
+    return 0;
+
+}
+
+int int sum_elements(int integers[], int length) {
+
+    return 0;
+
+}
+
--- main-2-3.cpp
+++ main-2-3.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 extern int sum_if_a_palindrome(int integers[], int length);
+extern bool is_a_palindrome(int integers[], int length);
+extern int sum_elements(int integers[], int length);
 
 int main() {
 

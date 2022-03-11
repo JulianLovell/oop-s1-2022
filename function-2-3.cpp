7895fb2332ea2438cea2987a31e7f70a8ff8a88f
JulianLovell <jlovell108@gmail.com>
2022-03-11 08:34:47 +0000

    practical2

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -6,13 +6,13 @@ int sum_if_a_palindrome(int integers[], int length) {
 
 }
 
-int bool is_a_palindrome(int integers[], int length) {
+bool is_a_palindrome(int integers[], int length) {
 
-    return 0;
+    return is_a_palindrome;
 
 }
 
-int int sum_elements(int integers[], int length) {
+int sum_elements(int integers[], int length) {
 
     return 0;
 

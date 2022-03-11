3053d74a84c3f87c6bfe805de5b3c9002ec68442
JulianLovell <jlovell108@gmail.com>
2022-03-11 08:12:58 +0000

    practical2

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -4,7 +4,7 @@
 int binary_to_number(int binary_digits[], int number_of_digits) {
 
     int x = 0;
-    int binary_to_number = 0
+    int binary_to_number = 0;
 
     for (int i = number_of_digits -1; i >= 0; i--) {
 

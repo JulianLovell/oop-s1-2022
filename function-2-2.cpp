24a5a30f25ba1f75b9e211db56440eae93183339
JulianLovell <jlovell108@gmail.com>
2022-03-11 08:08:36 +0000

    practical2

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <math.h>
 
 int binary_to_number(int binary_digits[], int number_of_digits) {
 
-    return 0;
+    int x = 0;
+    int binary_to_number = 0
+
+    for (int i = number_of_digits -1; i >= 0; i--) {
+
+        binary_to_number = binary_to_number + (binary_digits[i]*(pow(2, x)));
+        x++;
+
+    }
+
+    return binary_to_number;
 
 }
 
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <iostream>
 
 extern int binary_to_number(int binary_digits[], int number_of_digits);
 
 int main() {
+
+    int array[7] = {1,0,1,0,0,1,1};
+    int number_of_digits = 7;
+
+    std::cout << binary_to_number(array, number_of_digits) << std::endl;
 
     return 0;
 

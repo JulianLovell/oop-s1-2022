2b6e3f2748777adbe172df48640ffaf37129829b
JulianLovell <jlovell108@gmail.com>
2022-03-25 01:02:04 +0000

    practical3

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,1 +1,11 @@
 #include <iostream>
+
+void cpyda(double *old_array,double *new_array,int length){
+
+    for (int i = 0; i < length; i++){
+
+        new_array[i] = old_array[i];
+
+    }
+
+}
--- main-1-4.cpp
+++ main-1-4.cpp
@@ -1,1 +1,17 @@
 #include <iostream>
+
+extern void cpyda(double *old_array,double *new_array,int length);
+
+int main(){
+
+    int length = 3;
+
+    double old_array[3] = {1, 2, 3};
+
+    double new_array[3];
+
+    cpyda(old_array, new_array, length);
+
+    return 0;
+
+}

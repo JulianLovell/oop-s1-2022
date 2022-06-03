ca21d2db18b2842e6e6e7c138e5ace4768d5009c
JulianLovell <jlovell108@gmail.com>
2022-06-03 01:57:09 +0000

    p4

--- Documents.h
+++ Documents.h
@@ -2,3 +2,22 @@
 #include <string>
 
 using namespace std;
+
+class Document{
+    protected:
+
+     string title;
+     string documentType;
+     int numPages;
+
+     Document(string t, string d, int n);
+
+    void set_title (string t);
+    string get_title();
+
+    void set_documentType(string d);
+    string get_documentType();
+
+    void set_numPages(int n);
+    int get_numPages();
+};
